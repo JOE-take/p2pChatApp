920f11d61b1104253f7b6e5f1e32a6f966111357
joe <okoshi811@gmail.com>
2023-05-17 15:15:02 +0900

    テキスト表示、送信にbubbleを利用するように

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
-	"time"
 
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
@@ -72,52 +77,101 @@ func main() {
 func handleStream(stream network.Stream){
 	fmt.Println("handle!!!!")
 	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
-	go readData(rw)
-	go writeData(rw)	
+	bubble(rw)
 }
 
-func readData(rw *bufio.ReadWriter){
-	/*
-	for {
-		str, err := rw.ReadString('\n')
-		if err != nil {
-			panic(err)
-		}
 
-		if str == "quit" {
-			return
-		}
+func bubble(rw *bufio.ReadWriter) {
+	p := tea.NewProgram(initialModel(rw))
 
-		fmt.Printf("\x1b[32m%s\x1b[0m", str)
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
 	}
-	*/
-	fmt.Println("read")
-	for {
-			buf := make([]byte, 100)
-			rw.Read(buf)
-			fmt.Printf("\x1b[32m%s\x1b[0m> ", string(buf))
-			time.Sleep(time.Second)
-		}
 }
 
-func writeData(rw *bufio.ReadWriter) {
-	/*
-	_, err := rw.WriteString("Hello!")
-	if err != nil {
-		panic(err)
-	}
-	*/
-	fmt.Println("write")
-	stdReader := bufio.NewReader(os.Stdin)
+type (
+	errMsg error
+)
 
-	for {
-		fmt.Print("> ")
-		sendData, err := stdReader.ReadString('\n')
-		if err != nil {
-			panic(err)
+type model struct {
+	viewport    viewport.Model
+	messages    []string
+	textarea    textarea.Model
+	senderStyle lipgloss.Style
+	err         error
+	readWriter 	*bufio.ReadWriter
+}
+
+func initialModel(rw *bufio.ReadWriter) model {
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+
+	ta.Prompt = "┃ "
+	ta.CharLimit = 280
+
+	ta.SetWidth(30)
+	ta.SetHeight(3)
+
+	// Remove cursor line styling
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+
+	ta.ShowLineNumbers = false
+
+	vp := viewport.New(30, 5)
+	vp.SetContent(`Welcome to the chat room!
+Type a message and press Enter to send.`)
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		textarea:    ta,
+		messages:    []string{},
+		viewport:    vp,
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+	rw := m.readWriter
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			fmt.Println(m.textarea.Value())
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
+			m.viewport.SetContent(strings.Join(m.messages, "\n"))
+			m.textarea.Reset()
+			m.viewport.GotoBottom()
 		}
 
-		rw.WriteString(fmt.Sprintf("%s\n", sendData))
-		rw.Flush()
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
 	}
+
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s\n\n%s",
+		m.viewport.View(),
+		m.textarea.View(),
+	) + "\n\n"
 }
